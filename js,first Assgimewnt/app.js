a2c1bbf7e235a1f1c3b77675a8fa972ac9df6b4c
goharabbas313 <123669302+goharabbas313@users.noreply.github.com>
2023-01-27 01:33:58 -0800

    Add files via upload
    javascript assgiment

---
+++ js,first Assgimewnt/app.js
@@ -0,0 +1,23 @@
+//  my first assgiment javascript Chapter 1
+//         Question 1
+        
+        alert("Aos");
+
+
+       //  Question 2
+        alert("Error| plaase entera valid passward");
+        
+       //  Question 3
+        alert("welcome to js land \n happy conding");
+
+       //  Question 4
+        alert("welcome to js land.....");
+
+
+        var Name = ("i am gohar abbas \n i live in karachi \n i have done intermediate");
+
+        alert(Name);
+
+        document.write(Name);
+
+        console.log(Name);
